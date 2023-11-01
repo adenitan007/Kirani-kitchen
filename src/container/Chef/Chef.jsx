dafbf26f7915bfa9e172f877fb9685f608d643c3
adenitan <adenitanb@gmail.com>
2023-11-02 00:48:09 +0100

    Changes to be committed: 	new file:   src/assets/menu22.jpg 	modified:   src/assets/sign.png 	new file:   src/assets/sign1.png 	modified:   src/container/Chef/Chef.css 	modified:   src/container/Chef/Chef.jsx 	modified:   src/container/Intro/Intro.jsx

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import {images} from '../../constants';
-import {subHeading} from '../../constants'
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div className='app__bg app__wrapper section__padding'>
+  <div className="app__bg app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse">
-      <img src={images.chef} alt="chef" />
+      <img src={images.chef} alt="chef_image" />
     </div>
-
     <div className="app__wrapper_info">
-      <subHeading title="Chef's Word" />
-      <h1 className="headtext_cormorant">What we believe in</h1>
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">"As the heart and soul of our kitchen, I strive to create an unforgettable culinary journey that tantalizes the senses and nourishes the spirit. Join me on this flavorful adventure.".</p>
+        </div>
+        <p className="p__opensans"> we believe in crafting more than just meals; we craft moments that resonate with passion, creativity, and the joy of sharing. Our culinary artistry is a testament to our commitment to excellence and the love we pour into every dish." </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Saadat Aro-Lambo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
     </div>
   </div>
 );
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-
+import { BsFillPlayFill, BsPauseFill } from 'react-icons';
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo,setPlayVideo] = React.useState(false)
+  const VidRef = React.useRef();
+  const handleVideo = ( )=> {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+    
+  }
+  return (
+    < div className='app__video' >
+      <video src={meal} ref={VidRef} type='video/mp4' loop controls={false} muted />
+      <div className="app__video-overlay flex__center">
+        <div className="app__video-overlay_cicle flex__center" onclick>
+
+        </div>
+      </div>
+    </div >
+  )
+}
+
+
 
 export default Intro;
