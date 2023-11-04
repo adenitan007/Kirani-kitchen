e9a77b9803458a91211db0b1457934a159eb6417
adenitan <adenitanb@gmail.com>
2023-11-04 01:31:44 +0100

    modified:   src/components/Footer/FooterOverlay.css 	modified:   src/components/Footer/FooterOverlay.jsx 	modified:   src/components/Footer/Newsletter.css 	modified:   src/components/Footer/Newsletter.jsx 	modified:   src/components/Navbar/Navbar.jsx 	modified:   src/constants/images.js 	modified:   src/container/AboutUs/AboutUs.jsx 	modified:   src/container/Findus/FindUs.jsx 	modified:   src/container/Footer/Footer.css 	modified:   src/container/Footer/Footer.jsx

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">"subscribe to our newsletter and be the first to know!"</h1>
+      <p className="p__opensans">"Don't miss out on exclusive updates and exciting offers!"</p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder=" email address" />
+      <button type="button" className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -5,7 +5,7 @@ import './AboutUs.css';
 const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about'>
     <div className="app__aboutus-overlay flex__center">
-      <img src={images.G} alt="letter" />
+      {/* <img src={images.} alt="letter" /> */}
     </div>
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
