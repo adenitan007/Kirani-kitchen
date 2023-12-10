b4dbab0e806d351017b6459603544fb84fcc923d
adenitan <adenitanb@gmail.com>
2023-12-10 22:50:31 +0100

    modified:   src/constants/data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,5 +1,3 @@
-import images from './images';
-
 const special_soups = [
   {
     title: 'Turkey Peppersoup',
@@ -17,30 +15,30 @@ const special_soups = [
     tags: '3 LTRS | Special',
   },
   {
-   title: 'Ofada Stew',
+    title: 'Ofada Stew',
     price: '#15K',
     tags: '3 LTRS | Special',
   },
   {
-   title: 'Egusi',
+    title: 'Egusi',
     price: '#15K',
     tags: '3 LTRS | Soup',
   },
-  
- {
-   title: 'Seafood Okro',
+
+  {
+    title: 'Seafood Okro',
     price: '#16K',
     tags: '3 LTRS | Soup',
   },
-   {
-   title: 'Ewudu & Gbegiri',
+  {
+    title: 'Ewudu & Gbegiri',
     price: '#10K',
     tags: '3 LTRS | Soup',
   },
 ];
 
 const Protein_Pasta = [
-    {
+  {
     title: 'Turkey Peppersoup',
     price: '#13K',
     tags: '3 LTRS | Special',
@@ -56,49 +54,28 @@ const Protein_Pasta = [
     tags: '3 LTRS | Special',
   },
   {
-   title: 'Ofada Stew',
+    title: 'Ofada Stew',
     price: '#15K',
     tags: '3 LTRS | Special',
   },
   {
-   title: 'Egusi',
+    title: 'Egusi',
     price: '#15K',
     tags: '3 LTRS | Soup',
   },
-  
- {
-   title: 'Seafood Okro',
+
+  {
+    title: 'Seafood Okro',
     price: '#16K',
     tags: '3 LTRS | Soup',
   },
-   {
-   title: 'Ewudu & Gbegiri',
+  {
+    title: 'Ewudu & Gbegiri',
     price: '#10K',
     tags: '3 LTRS | Soup',
   },
 ];
 
-const awards = [
-  {
-    imgUrl: images.award02,
-    title: 'Bib Gourmond',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award01,
-    title: 'Rising Star',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award05,
-    title: 'AA Hospitality',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award03,
-    title: 'Outstanding Chef',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-];
 
-export default { special_soups, Protein_Pasta, awards };
+
+export default { special_soups, Protein_Pasta };
