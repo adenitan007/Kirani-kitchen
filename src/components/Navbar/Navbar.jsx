bda6f20d73cfc5a54a80a773b2368e783752ce74
adenitan <adenitanb@gmail.com>
2023-12-10 21:50:11 +0100

    modified:   src/components/Navbar/Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <img src={images.kirani} alt="app__logo" />
       </div>
       <ul className="app__navbar-links">
-        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#home">Home now</a></li>
         <li className="p__opensans"><a href="#about">About</a></li>
         <li className="p__opensans"><a href="#menu">Menu</a></li>
         <li className="p__opensans"><a href="#awards">Awards</a></li>
